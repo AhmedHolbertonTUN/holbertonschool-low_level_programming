5c478afd4349fc1a210c1716a1753b5473884c03
Jarraya Ahmed <1932@holbertonschool.com>
2020-06-16 02:04:13 +0000

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,7 +5,7 @@
  * print_to_98 - prints all integers from n to 98
  * @n: Integer
  *
- * Return: void
+ * Return: from n to 98
  */
 void print_to_98(int n)
 {
@@ -13,100 +13,100 @@ void print_to_98(int n)
 
 	if (n < 0)
 	{
-		{
-			a = abs(n);
-			for (i = a; i > 0; i--)
-			{
-				if (i < 10)
-				{
-					_putchar('-');
-					_putchar(i + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-				{
-					r = i / 10;
-					r1 = i % 10;
-					_putchar('-');
-					_putchar(r + '0');
-					_putchar(r1 + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-		}
-		for (i = 0; i < 98; i++)
-		{
-			if (i < 10)
-			{
-				_putchar(i + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				r = i / 10;
-				r1 = i % 10;
-				_putchar(r + '0');
-				_putchar(r1 + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('9');
-		_putchar('8');
-		_putchar('\n');
-	}
-	else if ((n >= 0) && (n <= 98))
 	{
-		{
-			for (i = n; i < 98; i++)
-			{
-				if (i < 10)
-				{
-					_putchar(i + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-				{
-					r = i / 10;
-					r1 = i % 10;
-					_putchar(r + '0');
-					_putchar(r1 + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-		}
-		_putchar('9');
-		_putchar('8');
-		_putchar('\n');
+	a = abs(n);
+	for (i = a; i > 0; i--)
+	{
+	if (i < 10)
+	{
+	_putchar('-');
+	_putchar(i + '0');
+	_putchar(',');
+	_putchar(' ');
 	}
 	else
 	{
-		for (i = n; i >= 99; i--)
-		{
-			if (i >= 100)
-			{
-				r = i / 100;
-				r1 = (i % 100) / 10;
-				r2 = i % 10;
-				_putchar(r + '0');
-				_putchar(r1 + '0');
-				_putchar(r2 + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
+	r = i / 10;
+	r1 = i % 10;
+	_putchar('-');
+	_putchar(r + '0');
+	_putchar(r1 + '0');
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
+	}
+	for (i = 0; i < 98; i++)
+	{
+	if (i < 10)
+	{
+	_putchar(i + '0');
+	_putchar(',');
+	_putchar(' ');
+	}
+	else
+	{
+	r = i / 10;
+	r1 = i % 10;
+	_putchar(r + '0');
+	_putchar(r1 + '0');
+	_putchar(',');
+	_putchar(' ');
+	}
+}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+	}
+	else if ((n >= 0) && (n <= 98))
+	{
+	{
+	for (i = n; i < 98; i++)
+	{
+	if (i < 10)
+	{
+	_putchar(i + '0');
+	_putchar(',');
+	_putchar(' ');
+	}
+	else
+	{
+	r = i / 10;
+	r1 = i % 10;
+	_putchar(r + '0');
+	_putchar(r1 + '0');
+	_putchar(',');
+	_putchar(' ');
+				}
+	}
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+	}
+	else
+	{
+	for (i = n; i >= 99; i--)
+	{
+	if (i >= 100)
+	{
+	r = i / 100;
+	r1 = (i % 100) / 10;
+	r2 = i % 10;
+	_putchar(r + '0');
+	_putchar(r1 + '0');
+	_putchar(r2 + '0');
+	_putchar(',');
+	_putchar(' ');
+	}
+	else
+	{
+	_putchar((i / 10) + '0');
+	_putchar((i % 10) + '0');
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
 		_putchar('9');
 		_putchar('8');
 		_putchar('\n');
