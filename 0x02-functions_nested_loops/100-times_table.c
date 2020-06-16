3919a22e3c6011645b55cbbb404c401c8492c799
Jarraya Ahmed <1932@holbertonschool.com>
2020-06-16 03:00:20 +0000

    100 times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,38 +9,40 @@
 
 void print_times_table(int n)
 {
+	int c, l, r1, r2;
+
 	if ((n >= 0) && (n <= 15))
 	{
 		for (c = 0; c <= n; c++)
 		{
-			putchar('0');
+			_putchar('0');
 			for (l = 1; l <= n; l++)
 			{
-				putchar(',');
-				putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 				r1 = c * l;
 				r2 = r1 / 10;
 				if (r1 < 10)
 				{
-					putchar(' ');
-					putchar(' ');
-					putchar(r1 + '0');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(r1 + '0');
 				}
 				else if ((r1 >= 10) && (r1 < 100))
 				{
-					putchar(' ');
-					putchar(r2 + '0');
-					putchar(r1 % 10 + '0');
+					_putchar(' ');
+					_putchar(r2 + '0');
+					_putchar(r1 % 10 + '0');
 				}
 				else
 				{
-					putchar(r2 / 10 + '0');
-					putchar((r1 / 10) % 10 + '0');
-					putchar(r1 % 10 + '0');
+					_putchar(r2 / 10 + '0');
+					_putchar((r1 / 10) % 10 + '0');
+					_putchar(r1 % 10 + '0');
 
 				}
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
