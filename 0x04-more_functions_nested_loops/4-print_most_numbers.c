b328c2e4477b5369d346a3100f29d081aee417a7
Jarraya Ahmed <1932@holbertonschool.com>
2020-06-18 08:03:18 +0000

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - prints the numbers from 0 to 9 expect 2 and 4
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a != 50 && a != 52)
+		{
+		_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
