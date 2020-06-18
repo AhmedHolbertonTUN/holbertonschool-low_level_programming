daf71bf672dc7ef02d0a26c1743a93caf5d815c9
Jarraya Ahmed <1932@holbertonschool.com>
2020-06-18 07:57:27 +0000

    multiple of a & b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - a function that multiplies two integers and returns the result.
+ *@a: integer
+ *@b: integer
+ *
+ * Return: a * b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
