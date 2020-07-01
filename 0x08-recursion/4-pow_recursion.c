03a6ee4187a27d3082707594984450e9d348fc89
Jarraya Ahmed <1932@holbertonschool.com>
2020-07-01 10:17:02 +0000

    power rec

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *_pow_recursion - function that returns the power of number
+ *@x: integer
+ *@y: integer
+ *Return: integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	else if (y < 0)
+		return (-1);
+	else
+		return (1);
+}
