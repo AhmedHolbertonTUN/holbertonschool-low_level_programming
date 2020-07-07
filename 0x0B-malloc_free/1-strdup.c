990c42522e6f5fef9efa64e9828726475b9a2a55
Jarraya Ahmed <1932@holbertonschool.com>
2020-07-07 17:50:54 +0000

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,13 +12,15 @@ char *_strdup(char *str)
 	int len, i;
 	char *s;
 
+	if (str == NULL)
+		return (NULL);
 	len = 0;
 	while (str[len] != '\0')
 	{
 		len++;
 	}
 	s = malloc(sizeof(char) * (len + 1));
-	if (s == NULL || str == NULL)
+	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < len; i++)
 		s[i] = str[i];
